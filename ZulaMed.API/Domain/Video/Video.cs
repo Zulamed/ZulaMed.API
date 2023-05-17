37badda9bcac552d064136147129ccbf5930cc5a
LoGiSTeRr <danikbor160@gmail.com>
2023-05-17 23:44:46 +0400

    Video.cs VideoId.cs VideoTitle.cs VideoUrl.cs
    added

---
+++ ZulaMed.API/Domain/Video/Video.cs
@@ -0,0 +1,8 @@
+namespace ZulaMed.API.Domain.Video;
+
+public class Video
+{
+    public VideoId Id { get; init; }
+    public VideoTitle VideoTitle { get; init; }
+    public VideoUrl VideoUrl { get; init; }
+}
