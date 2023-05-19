9dd63addf74956f689c5fa81916bccc3843da2b7
LoGiSTeRr <danikbor160@gmail.com>
2023-05-19 20:01:30 +0400

    thumbnail url validation

--- ZulaMed.API/Domain/Video/VideoThumbnail.cs
+++ ZulaMed.API/Domain/Video/VideoThumbnail.cs
@@ -7,6 +7,6 @@ public readonly partial struct VideoThumbnail
 {
     private static Validation Validate(string input)
     {
-        return !String.IsNullOrEmpty(input) ? Validation.Ok : Validation.Invalid("thumbnail url can't be empty");
+        return Uri.IsWellFormedUriString(input, UriKind.Absolute) ? Validation.Ok : Validation.Invalid("thumbnail url must to be valid");
     }
 }
