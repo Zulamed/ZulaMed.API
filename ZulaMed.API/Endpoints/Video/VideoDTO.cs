f31043bbd8f3960a6296df40a69f8d0ea1c3acc4
LoGiSTeRr <danikbor160@gmail.com>
2023-05-23 23:21:27 +0400

    implemented video getAll

---
+++ ZulaMed.API/Endpoints/Video/VideoDTO.cs
@@ -0,0 +1,11 @@
+namespace ZulaMed.API.Endpoints.Video;
+
+public class VideoDTO
+{
+    public required Guid Id { get; init; }
+    public required string VideoTitle { get; init; }
+    public required string VideoUrl { get; init; }
+    public required string VideoThumbnail { get; init; }
+    public required string VideoDescription { get; init; }
+    public required DateTime VideoPublishedDate { get; init; }
+}
