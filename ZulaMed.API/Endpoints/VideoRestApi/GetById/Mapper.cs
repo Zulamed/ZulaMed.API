84ea33386a6ce7263ded356b47cccb0b8814ed9c
LoGiSTeRr <danikbor160@gmail.com>
2023-05-25 23:40:11 +0400

    normal null check

--- ZulaMed.API/Endpoints/VideoRestApi/GetById/Mapper.cs
+++ ZulaMed.API/Endpoints/VideoRestApi/GetById/Mapper.cs
@@ -4,19 +4,16 @@ namespace ZulaMed.API.Endpoints.VideoRestApi.GetById;
 
 public static class Mapper
 {
-    public static Response ToResponse(this Video video)
+    public static VideoDTO ToResponse(this Video video)
     {
-        return new Response()
+        return new VideoDTO()
         {
-            Video = new VideoDTO()
-            {
-                Id = video.Id.Value,
+            Id = video.Id.Value,
                 VideoDescription = video.VideoDescription.Value,
                 VideoThumbnail = video.VideoThumbnail.Value,
                 VideoUrl = video.VideoUrl.Value,
                 VideoTitle = video.VideoTitle.Value,
                 VideoPublishedDate = video.VideoPublishedDate.Value
-            }
         };
     }
 }
