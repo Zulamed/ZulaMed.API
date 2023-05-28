ad6a982cf139915cef3f2a682afca61c1eb706d8
Seymur Bagirov <megadestroyer230@gmail.com>
2023-05-28 22:58:27 +0400

    remove GetByTitle endpoint and move it to Get endpoint

---
+++ ZulaMed.API/Endpoints/VideoRestApi/Get/GetAll/GetAllVideosQuery.cs
@@ -0,0 +1,25 @@
+using Mediator;
+using Microsoft.EntityFrameworkCore;
+using ZulaMed.API.Data;
+using ZulaMed.API.Domain.Video;
+
+namespace ZulaMed.API.Endpoints.VideoRestApi.Get.GetAll;
+
+public class GetAllVideosQuery : IQuery<Video[]>
+{
+}
+
+public class GetAllVideosQueryHandler : IQueryHandler<GetAllVideosQuery, Video[]>
+{
+    private readonly ZulaMedDbContext _context;
+
+    public GetAllVideosQueryHandler(ZulaMedDbContext context)
+    {
+        _context = context;
+    }
+    public async ValueTask<Video[]> Handle(GetAllVideosQuery query, CancellationToken cancellationToken)
+    {
+        var videos = await _context.Set<Video>().ToArrayAsync(cancellationToken: cancellationToken);
+        return videos;
+    }
+}
