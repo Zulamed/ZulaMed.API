a1c68beb7bff0cac956d38da0dc6d93069c13b17
LoGiSTeRr <danikbor160@gmail.com>
2023-05-31 20:02:09 +0400

    changed to GeneratedRegex

--- ZulaMed.API/Domain/Video/VideoThumbnail.cs
+++ ZulaMed.API/Domain/Video/VideoThumbnail.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Vogen;
 
 namespace ZulaMed.API.Domain.Video;
@@ -5,9 +6,13 @@ namespace ZulaMed.API.Domain.Video;
 [ValueObject<string>(Conversions.EfCoreValueConverter)]
 public readonly partial struct VideoThumbnail
 {
+    
+    [GeneratedRegex(@"\/thumbnails\/[^\/]+\.[a-zA-Z]{3,4}", 
+        RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+    private static partial Regex ThumbnailValidationRegex();
+    
     private static Validation Validate(string input)
     {
-        string pattern = @"\/thumbnails\/[^\/]+\.[a-zA-Z]{3,4}";
-        return System.Text.RegularExpressions.Regex.IsMatch(input, pattern) ? Validation.Ok : Validation.Invalid("Thumbnail URL must be in the format '/thumbnails/filename.jpg'.");
+        return ThumbnailValidationRegex().IsMatch(input) ? Validation.Ok : Validation.Invalid("Thumbnail URL must be in the format '/thumbnails/filename.jpg'.");
     }
 }
