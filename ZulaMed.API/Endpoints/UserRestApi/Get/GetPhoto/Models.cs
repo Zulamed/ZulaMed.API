9650ef609ef247edaf92e5b1db91d8735be9d154
LoGiSTeRr <danikbor160@gmail.com>
2023-06-09 13:36:02 +0400

    get photo py id not fully implemented
    post request started to be writing

---
+++ ZulaMed.API/Endpoints/UserRestApi/Get/GetPhoto/Models.cs
@@ -0,0 +1,31 @@
+using FastEndpoints;
+using FluentValidation;
+using Mediator;
+
+namespace ZulaMed.API.Endpoints.UserRestApi.Get.GetPhoto;
+
+public class Response
+{
+    public required string PhotoUrl { get; init; }
+}
+
+public class Request
+{
+    public required Guid Id { get; init; }
+}
+
+public class RequestValidator : Validator<Request>
+{
+    public RequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Id is required");
+    }
+}
+
+public class GetUserPhotoQuery : IQuery<Response>
+{
+    public required Guid UserId { get; init; }
+}
+
