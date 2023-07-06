8fb58b71858b9ef713ed73f6224a5f2aca88b459
LoGiSTeRr <danikbor160@gmail.com>
2023-07-07 00:05:02 +0400

    user post implemented

---
+++ ZulaMed.API/Endpoints/UserRestApi/Post/Models.cs
@@ -0,0 +1,45 @@
+using FastEndpoints;
+using FluentValidation;
+using Vogen;
+using ZulaMed.API.Domain.User;
+
+namespace ZulaMed.API.Endpoints.UserRestApi.Post;
+
+public class Request
+{
+    public required string Email { get; init; }
+    public required int GroupId  { get; init; }
+    public required string Name  { get; init; }
+    public required string Surname  { get; init; }
+    public required string Country  { get; init; }
+    public required string City  { get; init; }
+    public required string University  { get; init; }
+    public required string WorkPlace  { get; init; }
+}
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.Email).NotEmpty();
+        RuleFor(x => x.GroupId).NotEmpty();
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Surname).NotEmpty();
+        RuleFor(x => x.Country).NotEmpty();
+        RuleFor(x => x.City).NotEmpty();
+        RuleFor(x => x.University).NotEmpty();
+        RuleFor(x => x.WorkPlace).NotEmpty();
+    }
+}
+
+public class CreateUserCommand : Mediator.ICommand<Result<User, ValueObjectValidationException>>
+{
+    public required string Email { get; init; }
+    public required int GroupId  { get; init; }
+    public required string Name  { get; init; }
+    public required string Surname  { get; init; }
+    public required string Country  { get; init; }
+    public required string City  { get; init; }
+    public required string University  { get; init; }
+    public required string WorkPlace  { get; init; }
+}
