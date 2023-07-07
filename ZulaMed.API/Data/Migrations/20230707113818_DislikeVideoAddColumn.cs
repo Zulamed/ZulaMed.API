266f5c59140332d75eb79f196a6440335bdb68d8
Seymur Bagirov <megadestroyer230@gmail.com>
2023-07-07 15:38:44 +0400

    add migrations

---
+++ ZulaMed.API/Data/Migrations/20230707113818_DislikeVideoAddColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZulaMed.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DislikeVideoAddColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VideoDislike",
+                table: "Video",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoDislike",
+                table: "Video");
+        }
+    }
+}
