dc3a3cbebe9df206228cfc696a97c8a384b7e149
LoGiSTeRr <danikbor160@gmail.com>
2023-07-18 15:58:59 +0400

    migration added

---
+++ ZulaMed.API/Data/Migrations/20230718113309_UserSubscriptions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZulaMed.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserSubscriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserUser",
+                columns: table => new
+                {
+                    SubscribersId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubscriptionsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUser", x => new { x.SubscribersId, x.SubscriptionsId });
+                    table.ForeignKey(
+                        name: "FK_UserUser_User_SubscribersId",
+                        column: x => x.SubscribersId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUser_User_SubscriptionsId",
+                        column: x => x.SubscriptionsId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUser_SubscriptionsId",
+                table: "UserUser",
+                column: "SubscriptionsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserUser");
+        }
+    }
+}
