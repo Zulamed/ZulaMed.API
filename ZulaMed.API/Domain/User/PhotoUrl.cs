8001e67cc23b3677532295ee50c9d372c312e794
LoGiSTeRr <danikbor160@gmail.com>
2023-07-25 22:30:43 +0400

    photo url added to domain. Endpoint edited.

---
+++ ZulaMed.API/Domain/User/PhotoUrl.cs
@@ -0,0 +1,12 @@
+using Vogen;
+
+namespace ZulaMed.API.Domain.User;
+
+[ValueObject<string>(Conversions.EfCoreValueConverter)]
+public readonly partial struct PhotoUrl
+{
+    private static Validation Validate(string input)
+    {
+        return !string.IsNullOrWhiteSpace(input) ? Validation.Ok : Validation.Invalid("PhotoUrl can't be empty");
+    }
+}
