d3ab93257475cae162ba6254b4c7faf79140bc0b
Seymur Bagirov <megadestroyer230@gmail.com>
2023-08-03 17:04:01 +0400

    reorder

--- ZulaMed.API/Endpoints/VideoRestApi/Post/Endpoint.cs
+++ ZulaMed.API/Endpoints/VideoRestApi/Post/Endpoint.cs
@@ -16,7 +16,7 @@ using VoException = Vogen.ValueObjectValidationException;
 
 namespace ZulaMed.API.Endpoints.VideoRestApi.Post;
 
-public class CreateVideoCommandHandler 
+public class CreateVideoCommandHandler
     : Mediator.ICommandHandler<CreateVideoCommand, Result<Video, VoException>>
 {
     private readonly IAmazonS3 _s3Client;
@@ -25,7 +25,8 @@ public class CreateVideoCommandHandler
     private readonly IOptions<SqsQueueOptions> _sqsOptions;
     private readonly ZulaMedDbContext _dbContext;
 
-    public CreateVideoCommandHandler(ZulaMedDbContext dbContext, IAmazonS3 s3Client, IOptions<S3BucketOptions> s3Options, IAmazonSQS sqs, IOptions<SqsQueueOptions> sqsOptions)
+    public CreateVideoCommandHandler(ZulaMedDbContext dbContext, IAmazonS3 s3Client,
+        IOptions<S3BucketOptions> s3Options, IAmazonSQS sqs, IOptions<SqsQueueOptions> sqsOptions)
     {
         _dbContext = dbContext;
         _s3Client = s3Client;
@@ -38,11 +39,29 @@ public class CreateVideoCommandHandler
         CancellationToken cancellationToken)
     {
         var dbSet = _dbContext.Set<Video>();
+        var guid = Guid.NewGuid();
         try
         {
+            var user = await _dbContext.Set<User>().FirstOrDefaultAsync(x => (Guid)x.Id == command.VideoPublisherId,
+                cancellationToken: cancellationToken);
+            if (user is null)
+            {
+                throw new VoException("User not Found");
+            }
+
             var fileExtension = Path.GetExtension(command.Video.FileName);
-            var guid = Guid.NewGuid();
-            var response = await _s3Client.PutObjectAsync(new PutObjectRequest
+            var entity = await dbSet.AddAsync(new Video
+            {
+                Id = (VideoId)guid,
+                VideoDescription = (VideoDescription)command.VideoDescription,
+                VideoPublishedDate = (VideoPublishedDate)DateTime.UtcNow,
+                VideoThumbnail = (VideoThumbnail)command.VideoThumbnail,
+                VideoTitle = (VideoTitle)command.VideoTitle,
+                VideoUrl = (VideoUrl)(_s3Options.Value.BaseUrl + $"/videos/{guid}"),
+                Publisher = user
+            }, cancellationToken);
+
+            await _s3Client.PutObjectAsync(new PutObjectRequest
             {
                 BucketName = _s3Options.Value.BucketName,
                 Key = $"videos/{guid}",
@@ -54,33 +73,25 @@ public class CreateVideoCommandHandler
                     ["x-amz-meta-extension"] = fileExtension
                 },
             }, cancellationToken);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            
             await _sqs.SendMessageAsync(await GetSqsUrl(),
                 JsonSerializer.Serialize(new { VideoS3Path = $"videos/{guid}" }), cancellationToken);
             
-            var user = await _dbContext.Set<User>().FirstOrDefaultAsync(x => (Guid)x.Id == command.VideoPublisherId, cancellationToken: cancellationToken);
-            if (user is null)
-            {
-                throw new VoException("User not Found");
-            }
-            var entity = await dbSet.AddAsync(new Video
-            {
-                Id = (VideoId)guid,
-                VideoDescription = (VideoDescription)command.VideoDescription,
-                VideoPublishedDate = (VideoPublishedDate)DateTime.UtcNow,
-                VideoThumbnail = (VideoThumbnail)command.VideoThumbnail,
-                VideoTitle = (VideoTitle)command.VideoTitle,
-                VideoUrl = (VideoUrl)(_s3Options.Value.BaseUrl + $"/videos/{guid}"),
-                Publisher = user
-            }, cancellationToken);
-            await _dbContext.SaveChangesAsync(cancellationToken);
             return entity.Entity;
+        }
+        catch (DbUpdateException)
+        {
+            await _s3Client.DeleteObjectAsync(_s3Options.Value.BucketName, $"videos/{guid}", cancellationToken);
+            return new Error<VoException>(new VoException("Error while saving to database"));
         }
         catch (VoException e)
         {
             return new Error<VoException>(e);
         }
     }
-    
+
     private async Task<string> GetSqsUrl()
     {
         var request = new GetQueueUrlRequest
@@ -89,7 +100,7 @@ public class CreateVideoCommandHandler
         };
         var response = await _sqs.GetQueueUrlAsync(request);
         return response.QueueUrl;
-    } 
+    }
 }
 
 public class Endpoint : Endpoint<Request, VideoDTO>
@@ -115,6 +126,7 @@ public class Endpoint : Endpoint<Request, VideoDTO>
             await SendOkAsync(value.MapToResponse(), ct);
             return;
         }
+
         AddError(error.Value.Message);
         await SendErrorsAsync(cancellation: ct);
     }
