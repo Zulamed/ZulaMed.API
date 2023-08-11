146ca1b016dcf09ad64272a9199dfd9d00798065
Seymur Bagirov <megadestroyer230@gmail.com>
2023-08-11 15:00:46 +0400

    change endpoint logic to handle new like table

--- ZulaMed.API/Endpoints/VideoRestApi/Like/Endpoint.cs
+++ ZulaMed.API/Endpoints/VideoRestApi/Like/Endpoint.cs
@@ -1,23 +1,42 @@
+using System.Runtime.InteropServices;
 using FastEndpoints;
 using Mediator;
 using Microsoft.EntityFrameworkCore;
 using OneOf;
 using OneOf.Types;
 using ZulaMed.API.Data;
+using ZulaMed.API.Domain.Like;
+using ZulaMed.API.Domain.Video;
 
 namespace ZulaMed.API.Endpoints.VideoRestApi.Like;
 
-public class LikeVideoCommand : Mediator.ICommand<OneOf<Success, Error<string>, NotFound>>
+public class LikeVideoCommand : Mediator.ICommand<OneOf<Success, UserAlreadyLiked, Error<string>, NotFound>>
 {
     public required Guid VideoId { get; init; }
+
+    public required Guid UserId { get; init; }
 }
-public class UnLikeVideoCommand : Mediator.ICommand<OneOf<Success, Error<string>, NotFound>>
+
+[StructLayout(LayoutKind.Sequential, Size = 1)]
+public struct UserDidNotLike
+{
+}
+
+public class UnLikeVideoCommand : Mediator.ICommand<OneOf<Success, UserDidNotLike, Error<string>, NotFound>>
 {
     public required Guid VideoId { get; init; }
+
+    public required Guid UserId { get; init; }
+}
+
+[StructLayout(LayoutKind.Sequential, Size = 1)]
+public struct UserAlreadyLiked
+{
 }
 
 public class
-    LikeVideoCommandHandler : Mediator.ICommandHandler<LikeVideoCommand, OneOf<Success, Error<string>, NotFound>>
+    LikeVideoCommandHandler : Mediator.ICommandHandler<LikeVideoCommand,
+        OneOf<Success, UserAlreadyLiked, Error<string>, NotFound>>
 {
     private readonly ZulaMedDbContext _dbContext;
 
@@ -27,25 +46,40 @@ public class
     }
 
 
-    public async ValueTask<OneOf<Success, Error<string>, NotFound>> Handle(LikeVideoCommand command,
+    public async ValueTask<OneOf<Success, UserAlreadyLiked, Error<string>, NotFound>> Handle(LikeVideoCommand command,
         CancellationToken cancellationToken)
     {
+        await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
         try
         {
+            var userAlreadyLiked = await _dbContext.Set<Like<Video>>()
+                .AnyAsync(x => (Guid)x.LikedBy.Id == command.UserId && (Guid)x.Parent.Id == command.VideoId,
+                    cancellationToken: cancellationToken);
+            if (userAlreadyLiked)
+            {
+                return new UserAlreadyLiked();
+            }
+
             var rows = await _dbContext.Database.ExecuteSqlAsync(
-                $"""UPDATE "Video" SET "VideoLike" = "VideoLike" + 1 WHERE "Id" = {command.VideoId}""", 
+                $"""UPDATE "Video" SET "VideoLike" = "VideoLike" + 1 WHERE "Id" = {command.VideoId}""",
                 cancellationToken: cancellationToken);
+            await _dbContext.Database.ExecuteSqlAsync
+            ($"""INSERT INTO "LikeVideo" VALUES ({Guid.NewGuid()}, {DateTime.UtcNow}, {command.VideoId}, {command.UserId})""",
+                cancellationToken: cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
             return rows > 0 ? new Success() : new NotFound();
         }
         catch (Exception e)
         {
+            await transaction.RollbackAsync(cancellationToken);
             return new Error<string>(e.Message);
         }
     }
 }
 
 public class
-    UnLikeVideoCommandHandler : Mediator.ICommandHandler<UnLikeVideoCommand, OneOf<Success, Error<string>, NotFound>>
+    UnLikeVideoCommandHandler : Mediator.ICommandHandler<UnLikeVideoCommand,
+        OneOf<Success, UserDidNotLike, Error<string>, NotFound>>
 {
     private readonly ZulaMedDbContext _dbContext;
 
@@ -53,19 +87,33 @@ public class
     {
         _dbContext = dbContext;
     }
-    
-    public async ValueTask<OneOf<Success, Error<string>, NotFound>> Handle(UnLikeVideoCommand command,
+
+    public async ValueTask<OneOf<Success, UserDidNotLike, Error<string>, NotFound>> Handle(UnLikeVideoCommand command,
         CancellationToken cancellationToken)
     {
+        await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
         try
         {
+            var didUserLike = await _dbContext.Set<Like<Video>>()
+                .AnyAsync(x => (Guid)x.LikedBy.Id == command.UserId && (Guid)x.Parent.Id == command.VideoId,
+                    cancellationToken: cancellationToken);
+            if (!didUserLike)
+            {
+                return new UserDidNotLike();
+            }
+
             var rows = await _dbContext.Database.ExecuteSqlAsync(
-                $"""UPDATE "Video" SET "VideoLike" = GREATEST("VideoLike" - 1, 0) WHERE "Id" = {command.VideoId}""", 
+                $"""UPDATE "Video" SET "VideoLike" = GREATEST("VideoLike" - 1, 0) WHERE "Id" = {command.VideoId}""",
                 cancellationToken: cancellationToken);
+            await _dbContext.Database.ExecuteSqlAsync(
+                $"""DELETE FROM "LikeVideo" WHERE "ParentId" = {command.VideoId} AND "UserId" = {command.UserId}""",
+                cancellationToken: cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
             return rows > 0 ? new Success() : new NotFound();
         }
         catch (Exception e)
         {
+            await transaction.RollbackAsync(cancellationToken);
             return new Error<string>(e.Message);
         }
     }
@@ -82,11 +130,11 @@ public class Endpoint : Endpoint<Request>
 
     public override void Configure()
     {
-        Verbs(Http.POST, Http.DELETE);
         Routes("/video/{id}/like");
+        Verbs(Http.POST, Http.DELETE);
         AllowAnonymous();
         // for some reason FastEndpoints was sending 415, this clears the defaults so that it wouldn't send it 
-        Description(b => { }, clearDefaults: true); 
+        Description(b => { }, clearDefaults: true);
     }
 
 
@@ -94,9 +142,14 @@ public class Endpoint : Endpoint<Request>
     {
         if (HttpContext.Request.Method == Http.POST.ToString())
         {
-            var result = await _mediator.Send(new LikeVideoCommand { VideoId = req.Id }, ct);
+            var result = await _mediator.Send(new LikeVideoCommand { VideoId = req.Id, UserId = req.UserId }, ct);
             await result.Match(
                 s => SendOkAsync(ct),
+                ua =>
+                {
+                    AddError("User already liked this video");
+                    return SendErrorsAsync(cancellation: ct);
+                },
                 e => SendAsync(new
                 {
                     StatusCode = 500,
@@ -107,9 +160,14 @@ public class Endpoint : Endpoint<Request>
         }
         else if (HttpContext.Request.Method == Http.DELETE.ToString())
         {
-            var result = await _mediator.Send(new UnLikeVideoCommand { VideoId = req.Id }, ct);
+            var result = await _mediator.Send(new UnLikeVideoCommand { VideoId = req.Id, UserId = req.Id }, ct);
             await result.Match(
                 s => SendOkAsync(ct),
+                ue =>
+                {
+                    AddError("User did not like this video");
+                    return SendErrorsAsync(cancellation: ct);
+                },
                 e => SendAsync(new
                 {
                     StatusCode = 500,
--- ZulaMed.API/Endpoints/VideoRestApi/Like/Models.cs
+++ ZulaMed.API/Endpoints/VideoRestApi/Like/Models.cs
@@ -1,7 +1,21 @@
+using FastEndpoints;
+using FluentValidation;
+
 namespace ZulaMed.API.Endpoints.VideoRestApi.Like;
 
 
 public class Request
 {
     public Guid Id { get; init; }
+    
+    public Guid UserId { get; init; }
+}
+
+public class RequestValidator : Validator<Request>
+{
+    public RequestValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.UserId).NotEmpty();
+    } 
 }
