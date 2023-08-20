0143b0d23d177ca10692b914002b0c3f6623e146
LoGiSTeRr <danikbor160@gmail.com>
2023-08-20 19:43:24 +0400

    UniversityUser domain added

---
+++ ZulaMed.API/Domain/UniversityUser/UniversityUser.cs
@@ -0,0 +1,9 @@
+namespace ZulaMed.API.Domain.UniversityUser;
+
+public class UniversityUser
+{
+    public required User.User User { get; init; }
+    public required UserAddress UserAddress { get; init; }
+    public required UserPostCode UserPostCode { get; init; }
+    public required UserPhone UserPhone { get; init; }
+} 
---
+++ ZulaMed.API/Domain/UniversityUser/UserPostCode.cs
@@ -0,0 +1,12 @@
+using Vogen;
+
+namespace ZulaMed.API.Domain.UniversityUser;
+
+[ValueObject<string>(Conversions.EfCoreValueConverter)]
+public readonly partial struct UserPostCode
+{
+    private static Validation Validate(string input)
+    {
+        return !string.IsNullOrWhiteSpace(input) ? Validation.Ok : Validation.Invalid("PostalCode can't be empty");
+    }
+}
