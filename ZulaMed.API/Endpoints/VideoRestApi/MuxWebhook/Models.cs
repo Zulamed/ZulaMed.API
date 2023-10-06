1852f0442b484ec11ffd295c7b037d0d6bc70355
Seymur Bagirov <megadestroyer230@gmail.com>
2023-10-07 02:00:27 +0400

    add some changes to put endpoint and change metadata binding in webhook

--- ZulaMed.API/Endpoints/VideoRestApi/MuxWebhook/Models.cs
+++ ZulaMed.API/Endpoints/VideoRestApi/MuxWebhook/Models.cs
@@ -1,11 +1,22 @@
-using Mediator;
-using Mux.Csharp.Sdk.Model;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ZulaMed.API.Endpoints.VideoRestApi.MuxWebhook;
 
-public class Request 
+public class Metadata
+{
+    public required Guid VideoId { get; set; }
+}
+
+public class MuxData
+{
+    public Metadata? Metadata { get; set; }
+}
+
+public class Request
 {
     public required string Type { get; set; }
-    
-    public required Asset Data { get; set; }
+    public required Dictionary<string, JsonElement> Data { get; init; }
+
+    [JsonIgnore] public MuxData MuxData { get; set; } = new();
 }
