da1fd673546e3210cf8b7e12fb96ea5c361930b0
Seymur Bagirov <megadestroyer230@gmail.com>
2023-10-17 02:37:47 +0400

    verify webhook signature from Mux

--- ZulaMed.API/Endpoints/MuxWebhook/Models.cs
+++ ZulaMed.API/Endpoints/MuxWebhook/Models.cs
@@ -1,6 +1,8 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using FastEndpoints;
+using FluentValidation;
 using Mux.Csharp.Sdk.Model;
 
 namespace ZulaMed.API.Endpoints.MuxWebhook;
@@ -13,17 +15,31 @@ public class Metadata
 public class MuxData
 {
     public required Metadata Metadata { get; set; }
-    
-    public required Asset Data { get; set; } 
+
+    public required Asset Data { get; set; }
 }
 
-public record Request
+public record Request : IPlainTextRequest
 {
-    public required string Type { get; set; }
+    public required string Content { get; set; }
 
-    public required JsonObject Data { get; init; } = null!;
+    [FromHeader("mux-signature")] 
+    public string MuxSignature { get; set; } = null!;
+
+    [JsonIgnore] public string Type { get; set; }
+
+    [JsonIgnore] public JsonObject Data { get; set; } = null!;
 
     [JsonIgnore] public MuxData? MuxData { get; set; }
-    
+
     [JsonIgnore] public string Status { get; set; }
+}
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.MuxSignature).NotEmpty();
+        RuleFor(x => x.Content).NotEmpty();
+    }
 }
