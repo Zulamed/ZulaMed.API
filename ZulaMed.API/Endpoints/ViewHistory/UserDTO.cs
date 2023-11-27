ef0fd62c193660b5374d76a6fe069847588f3821
LoGiSTeRr <danikbor160@gmail.com>
2023-11-28 00:09:24 +0400

    history

--- ZulaMed.API/Endpoints/ViewHistory/UserDTO.cs
+++ ZulaMed.API/Endpoints/ViewHistory/UserDTO.cs
@@ -3,5 +3,6 @@ namespace ZulaMed.API.Endpoints.ViewHistory;
 public class UserDTO
 {
     public required Guid Id { get; init; }
-    public required string Login { get; init; }
+    public required string Username { get; init; }
+    public required string UserProfileUrl { get; init; }
 }
--- ZulaMed.API/Endpoints/ViewHistory/VideoDTO.cs
+++ ZulaMed.API/Endpoints/ViewHistory/VideoDTO.cs
@@ -4,8 +4,10 @@ public class VideoDTO
 {
     public required Guid Id { get; init; }
     public string? VideoTitle { get; init; }
+    public string? VideoUrl { get; init; }
     public required Guid VideoPublisherId { get; init; }
     public string? VideoThumbnail { get; init; }
+    public DateTime VideoDate { get; init; }
     public string? VideoDescription { get; init; }
     public required long VideoViews { get; init; }
 }
