07c800f8071cf48383300e60635d2b528fc69ef3
LoGiSTeRr <danikbor160@gmail.com>
2023-11-28 00:09:24 +0400

    history

--- ZulaMed.API/Endpoints/ViewHistory/Get/Mapper.cs
+++ ZulaMed.API/Endpoints/ViewHistory/Get/Mapper.cs
@@ -10,14 +10,17 @@ public static class Mapper
             ViewedBy = new UserDTO
             {
                 Id = viewHistory.ViewedBy.Id.Value,
-                Login = viewHistory.ViewedBy.Login.Value
+                Username = viewHistory.ViewedBy.Login.Value,
+                UserProfileUrl = viewHistory.ViewedBy.PhotoUrl?.Value!
             },
             ViewedVideo = new VideoDTO
             {
                 Id = viewHistory.ViewedVideo.Id.Value,
                 VideoTitle = viewHistory.ViewedVideo.VideoTitle?.Value,
+                VideoUrl = viewHistory.ViewedVideo.VideoUrl?.Value,
                 VideoPublisherId = viewHistory.ViewedVideo.Publisher.Id.Value,
                 VideoThumbnail = viewHistory.ViewedVideo.VideoThumbnail?.Value,
+                VideoDate = viewHistory.ViewedVideo.VideoPublishedDate.Value,
                 VideoDescription = viewHistory.ViewedVideo.VideoDescription?.Value,
                 VideoViews = viewHistory.ViewedVideo.VideoView.Value
             },
