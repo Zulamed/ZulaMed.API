f7a64d490db22ea802907a448cfcbc679f3c8d3d
Seymur Bagirov <megadestroyer230@gmail.com>
2024-01-04 20:30:47 +0400

    add migration files

---
+++ ZulaMed.API/Data/Migrations/20240104162244_AddBannerUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZulaMed.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannerUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BannerUrl",
+                table: "User",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannerUrl",
+                table: "User");
+        }
+    }
+}
